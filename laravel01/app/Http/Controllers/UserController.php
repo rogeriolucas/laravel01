20d7c19e3352086d2b5933d117a6cb1aca964141
ROGERIO LUCAS <rogeriolucas445@hotmail.com>
2022-01-31 15:58:02 -0300

    vs icon

---
+++ laravel01/app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Htpp\Controllers;
+use Illuminate\Http\Request;
+
+class UserController extends Controllers
+{
+
+ public function listuser()
+{
+   echo "<h1>Listagem de Usuario </h1>";
+
+}
+ 
+
+}
